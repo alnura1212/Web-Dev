2d52f57152a74c022db8d09c27dc21a5325ecb3b
alnura1212 <156894643+alnura1212@users.noreply.github.com>
2025-02-14 01:04:58 +0500

    Update 4.js

--- lab3/first/data_types/4.js
+++ lab3/first/data_types/4.js
@@ -1,13 +1,5 @@
-function readNumber() {
-  let num;
-
-  do {
-    num = prompt("Enter a number please?", 0);
-  } while ( !isFinite(num) );
-
-  if (num === null || num === '') return null;
-
-  return +num;
+let i = 0;
+while (i < 11) {
+  i += 0.2;
+  if (i > 9.8 && i < 10.2) alert( i );
 }
-
-alert(`Read: ${readNumber()}`);
