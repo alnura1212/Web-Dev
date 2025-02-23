79a2258f740b0f707a98d79b522fade7b23b4394
alnura1212 <a_amrenova@kbtu.kz>
2025-02-23 21:53:38 +0500

    Initial commit

--- lab4/src/app/app.component.ts
+++ lab4/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from "./products/products.component";
-import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ProductsComponent],
+  imports: [ProductsComponent, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shop';
+  categories = ['Игрушки', 'Подгузники', 'Питание', 'Гаджеты'];
 }
