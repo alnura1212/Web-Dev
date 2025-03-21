b4acebd685fc8e83bc1597bcf38b380842dfaccc
alnura1212 <156894643+alnura1212@users.noreply.github.com>
2025-03-22 00:22:17 +0500

    Create a.py

---
+++ lab7/task1/1/a.py
@@ -0,0 +1,27 @@
+import math
+a=int(input())
+b=int(input())
+c=math.sqrt(a**2 + b**2)
+print(c)
+
+
+a=int(input())
+print("The next number for the number ",a," is ", a+1,".")
+print("The previous number for the number ",a," is ",(a-1),".")
+
+
+a=int(input())
+b=int(input())
+print (int(b/a))
+
+
+a=int(input())
+b=int(input())
+print (b%a)
+
+
+a=int(input())
+b=int(input())
+print((a*b)%109)
+
+
